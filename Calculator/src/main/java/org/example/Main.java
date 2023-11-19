d982fe419aa48e17ac1ae96a150b4bb5dfe0c739
lehnera <lehnera@technikum-wien.at>
2023-11-19 22:45:17 +0100

    Added ConsoleUI and basic user workflow

--- Calculator/src/main/java/org/example/Main.java
+++ Calculator/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calc = new Calculator();
+        UserInteractionHandler handler = new UserInteractionHandler(calc);
+        handler.Run();
     }
 }
---
+++ Calculator/src/main/java/org/example/UserInteractionHandler.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class UserInteractionHandler{
+
+    private Calculator calc;
+    private int operation;
+    private int firstParam;
+    private int secondParam;
+
+   private Scanner in = new Scanner(System.in);
+
+    public UserInteractionHandler(Calculator calculator){
+        calc = calculator;
+        operation = 0;
+        firstParam = 0;
+        secondParam = 0;
+    }
+
+    public void Run(){
+        welcome();
+        askParams();
+        displayResult();
+    }
+
+    private void welcome(){
+        System.out.print("Welcome to the calculator app, which operation do you want to perform? \n"+
+                "1. Addition \n" +
+                "2. Multiplication \n"+
+                "3. Division \n"+
+                "Your Choice: ");
+        operation = in.nextInt();
+    }
+
+    private void askParams(){
+      System.out.print("Please enter your first parameter (Only integer)\n" +
+              "First parameter: ");
+      firstParam = in.nextInt();
+      System.out.print("Please enter your second parameter (Only integer) \n"+
+              "Second parameter: ");
+      secondParam = in.nextInt();
+    }
+
+    private void displayResult(){
+        System.out.print("Your result is: ");
+        switch (operation){
+            case 1:
+                System.out.println(calc.add(firstParam,secondParam));
+                break;
+
+            case 2:
+                System.out.println(calc.multiply(firstParam,secondParam));
+                break;
+
+            case 3:
+                System.out.println(calc.divide(firstParam,secondParam));
+                break;
+
+            default:
+                System.out.print("Something went wrong, please retry and add insert only integers. "+
+                        "If the problem persits, please contact Application support.");
+                break;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
